406548da87561c2967689242bdf47fb4ce7fb114
Panagiotis Megas <it2022065@hua.gr>
2025-01-24 23:06:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 #include <sys/select.h>
 #include <sys/wait.h>
 
-#define max(x, y) ((x) > (y) ? (x) : (y))
 
 int N;
 int tmp = 0;
@@ -20,6 +19,7 @@ struct msg {
     int id;
     int val;
 };
+
 void kill_child(int sig){
     printf("\nCaught signal %d (SIGINT). Exiting gracefully...\n", sig);
     
@@ -50,7 +50,7 @@ void kill_child(int sig){
     free(pid);
     exit(0);
 }
-void ter_child(int sig){
+void term_child(int sig){
     printf("\nCaught signal %d (SIGINT). Exiting gracefully...\n", sig);
     // close pipes
     close(fd[tmp][0]);
@@ -76,7 +76,6 @@ int main(int argc, char *argv[]) {
         printf("The program takes 2 arguments\n1st The name of the output file\n2nd The number of workers in the worker pool (positive integer)\n");
         return 2;
     }
-
     char *name = argv[1];
     N = strtol(argv[2], NULL, 10);
 
@@ -85,8 +84,8 @@ int main(int argc, char *argv[]) {
     
 
     // Initialize pipes
-    fd = (int **)malloc((N + 1) * sizeof(int *));
-    cfd = (int **)malloc((N + 1) * sizeof(int *));
+    fd = (int **)malloc((N + 1) * sizeof(int *));//father writes
+    cfd = (int **)malloc((N + 1) * sizeof(int *));//child writes
     for (int i = 1; i <= N; i++) {
         fd[i] = (int *)malloc(2 * sizeof(int));
         cfd[i] = (int *)malloc(2 * sizeof(int));
@@ -111,7 +110,13 @@ int main(int argc, char *argv[]) {
             close(fd[i][0]);
             close(cfd[i][1]);
         }
-        signal(SIGINT, kill_child);
+        //signal(SIGINT, kill_child);
+
+        struct sigaction sa;
+        sa.sa_handler = kill_child; 
+        sa.sa_flags = 0; 
+        sigemptyset(&sa.sa_mask);
+        sigaction(SIGINT, &sa, NULL);
     }
     // close unnecessary pipes for child
     else {
@@ -121,14 +126,18 @@ int main(int argc, char *argv[]) {
             if (tmp != i) close(fd[i][0]);
             if(tmp!=i) close(fd[i][1]);
         }
-        signal(SIGINT, ter_child);
+
+        struct sigaction sa;
+        sa.sa_handler = term_child;  
+        sa.sa_flags = 0; 
+        sigemptyset(&sa.sa_mask);
+        sigaction(SIGINT, &sa, NULL);
     }
 
     // child logic
     if (a == 0) {
         while (1) {
             struct msg mes;
-            
             ssize_t read_bytes = read(fd[tmp][0], &mes, sizeof(mes));  // read task from parent
             if (read_bytes <= 0) {
                 printf("Worker %d: Failed to read from pipe\n", tmp);
@@ -153,8 +162,10 @@ int main(int argc, char *argv[]) {
         }
         
 
-    } else {
-        // parent logic
+    }
+    // parent logic 
+    else {
+       
         out = fopen(name, "w");
         fprintf(out, "pid %d from ppid %d\n", getpid(), getppid());
         fclose(out);
@@ -165,36 +176,20 @@ int main(int argc, char *argv[]) {
         }
 
         while (1) {
-            fd_set writefds;
-            FD_ZERO(&writefds);
-
-            // Check which workers are available
+            
+            // assign task to the first available worker
             for (int i = 1; i <= N; i++) {
-                if (av[i]) FD_SET(fd[i][1], &writefds);
-            }
-
-            struct timeval tv2 = {0, 5000};  // timeout for select
-            int r = select(FD_SETSIZE + 1, NULL, &writefds, NULL, &tv2);
-
-            if (r == -1) {
-                return 3;
-            }
-
-            if (r > 0) {
-                // assign task to the first available worker
-                for (int i = 1; i <= N; i++) {
-                    if (FD_ISSET(fd[i][1], &writefds)) {
-                        struct msg mes = {i, rand() % 5 + 1};  // generate task with random wait time
-                        ssize_t write_bytes = write(fd[i][1], &mes, sizeof(mes));
-                        if (write_bytes <= 0) {
-                            printf("Parent: Failed to write to worker %d\n", i);
-                        }
-                        av[i] = 0;  // mark worker as busy
-                        break;
+                if (av[i]) {
+                    struct msg mes = {i, rand() % 5 + 1};  // generate task with random wait time
+                    ssize_t write_bytes = write(fd[i][1], &mes, sizeof(mes));
+                    if (write_bytes <= 0) {
+                        printf("Parent: Failed to write to worker %d\n", i);
                     }
+                    av[i] = 0;  // mark worker as busy
+                    break;
                 }
             }
-
+            
             // Check for completion messages from workers
             fd_set rfds;
             FD_ZERO(&rfds);
